aa81961517ee5779ded2fff1c54236c0fa5600ba
Aleph Perth <95363770+AlephPerth@users.noreply.github.com>
2022-09-28 22:51:02 -0300

    Add files via upload

--- Proyecto-Final - HERNAN/JS/main.js
+++ Proyecto-Final - HERNAN/JS/main.js
@@ -22,25 +22,25 @@ function selectYourComputer () {
         pc.preciofinalPC = 31499
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalPC
+        p.innerHTML = 'PC diseño domestico ' + '$' + pc.preciofinalPC
 
     } else if (oficina.checked) {
         pc.preciofinalPC = 75382
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalPC
+        p.innerHTML = 'PC diseño oficina ' + '$' + pc.preciofinalPC
 
     } else if (gamer.checked) {
         pc.preciofinalPC = 169258
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalPC
+        p.innerHTML = 'PC diseño gamer ' + '$' + pc.preciofinalPC
 
     } else if (disenio.checked) {
         pc.preciofinalPC = 229999
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalPC
+        p.innerHTML = 'PC diseño multimedia ' + '$' + pc.preciofinalPC
     }
 
     let ryzen3 = document.getElementById('Ryzen3')
@@ -82,69 +82,84 @@ function selectYourComputer () {
         pc.preciofinalram = 13999
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalram
+        
+        p.innerHTML = 'Fury 16Gb Ddr4 3000Mhz ' + '$' + pc.preciofinalram
     } else if (ram32.checked) {
         pc.preciofinalram = 37900
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalram
+        p.innerHTML = 'Fury 32Gb Ddr4 3200Mhz ' + '$' + pc.preciofinalram
         
     } else if (ram64.checked) {
         pc.preciofinalram = 89817
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalram
+        p.innerHTML = 'Fury 64Gb Ddr4 3600Mhz ' + '$' + pc.preciofinalram
     } else if (ram128.checked) {
         pc.preciofinalram = 89817
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalram
+        p.innerHTML = 'Fury 128Gb Ddr5 128Gbz ' + '$' + pc.preciofinalram
 
     }
     let hdd128 = document.getElementById('hdd128')
     let hhd256 = document.getElementById('hhd256')
     let hhd512 = document.getElementById('hhd512')
+    let hhd1t = document.getElementById('hhd1t')
 
     if (hdd128.checked) {
         pc.preciofinaldisco = 4940
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinaldisco
+        p.innerHTML = 'Kingston Ssd 128Gb ' + '$' + pc.preciofinaldisco
 
     } else if (hhd256.checked) {
         pc.preciofinaldisco = 7778
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinaldisco
+        p.innerHTML = 'Kingston Ssd 256Gb ' + '$' + pc.preciofinaldisco
 
     } else if (hhd512.checked) {
+        pc.preciofinaldisco = 28611
+        let p = document.createElement('p')
+        campoPresupuesto.appendChild(p);
+        p.innerHTML = 'Kingston Ssd 1Tb ' + '$' + pc.preciofinaldisco
+
+    } else if (hhd1t.checked) {
         pc.preciofinaldisco = 8814
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinaldisco
+        p.innerHTML = 'Kingston Ssd 512Gb ' + '$' + pc.preciofinaldisco
     }
 
-    let NVidia_xx1 = document.getElementById('NVidia_xx1')
-    let NVidia_xx2 = document.getElementById('NVidia_xx2')
-    let NVidia_xx3 = document.getElementById('NVidia_xx3')
+    let nvidia_xx1 = document.getElementById('NVidia_xx1')
+    let nvidia_xx2 = document.getElementById('NVidia_xx2')
+    let nvidia_xx3 = document.getElementById('NVidia_xx3')
+    let nvidia_xx4 = document.getElementById('NVidia_xx4')
 
-    if (NVidia_xx1.checked) {
+    if (nvidia_xx1.checked) {
         pc.preciofinalvideo = 99552
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalvideo
+        p.innerHTML = 'Nvidia Xc Geforce RTX 2060 ' + '$' + pc.preciofinalvideo
 
-    } else if (NVidia_xx2.checked) {
+    } else if (nvidia_xx2.checked) {
         pc.preciofinalvideo = 89658
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalvideo
+        p.innerHTML = 'Nvidia Xc Geforce MSI 2060 ' + '$' + pc.preciofinalvideo
 
-    } else if (NVidia_xx3.checked) {
+    } else if (nvidia_xx3.checked) {
         pc.preciofinalvideo = 75149
         let p = document.createElement('p')
         campoPresupuesto.appendChild(p);
-        p.innerHTML = ' ' + '$' + pc.preciofinalvideo
+        p.innerHTML = 'Nvidia Xc Geforce RTX 1060 ' + '$' + pc.preciofinalvideo
+
+    } else if (nvidia_xx4.checked) {
+        pc.preciofinalvideo = 121999
+        let p = document.createElement('p')
+        campoPresupuesto.appendChild(p);
+        p.innerHTML = 'Nvidia Xc GeForce GTX 3060 ' + '$' + pc.preciofinalvideo
     }
 
     let publicarPrecio = document.getElementById('your-upgrade')
